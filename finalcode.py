c05a3ffa8dcedf419d613b0658a91cf51b368793
swikritisuman <122162710+swikritisuman@users.noreply.github.com>
2024-04-06 21:47:33 +0530

    Add files via upload

---
+++ finalcode.py
@@ -0,0 +1,76 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if 0 <= hour < 12:
+        speak("Good morning!")
+
+    elif 12 <= hour < 18:
+        speak("Good Afternoon")
+
+    else:
+        speak("Good evening")
+
+    speak("I am your AI assistant. Please tell me how may I help you")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognising...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    #while True:
+    if 1:
+        query = takeCommand().lower()
+
+        #logic for executing tasks based on query
+        if 'wikipedia' in query:
+             speak('searching wikipedia...')
+             query =query.replace("wikipedia","")
+             results = wikipedia.summary(query,sentences=2)
+             speak("According to wikipedia")
+             print(results)
+             speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open  google' in  query:
+            webbrowser.open("google.com")
+        elif 'open  Stackoverflow' in  query:
+            webbrowser.open("stackoverflow.com")
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strtime("%H:%M:%S")
+            speak(f"The time is {strTime}")
+        elif 'open code' in query:
+            codePath = "C:\\Users\\swikr\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
